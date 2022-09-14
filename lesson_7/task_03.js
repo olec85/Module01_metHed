ea0098d11a9b06f52312f94f167b90d60175e388
Davydov Konstantin <olec85@bk.ru>
2022-09-14 15:20:39 +0300

    урок 7 исправил задачу 2

--- lesson_7/task_03.js
+++ lesson_7/task_03.js
@@ -1,8 +1,13 @@
 
 const names = ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
-const namesMr = names.map(function(nam) {
-    return 'Mr ' + names
-})
 
-console.log(namesMr);//чот не то выводит подскажите где ошибка
+const newArray =names.map((item) => {
+    return 'Mr ' + item
+})
+console.log(newArray);
+// const namesMr = names.map(function(nam) {
+    // return 'Mr ' + names
+// })
+
+//console.log(namesMr);//чот не то выводит подскажите где ошибка
 
