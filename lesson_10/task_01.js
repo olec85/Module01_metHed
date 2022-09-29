9a54ba401f029d2935d701aab1dc5447bf66c9f3
Davydov Konstantin <olec85@bk.ru>
2022-09-29 18:23:34 +0300

    урок 10 сделал

--- lesson_10/task_01.js
+++ lesson_10/task_01.js
@@ -37,7 +37,7 @@ const cart = {
   clear() {
     this.items = [];
     this.count = 0;
-    // this.totalPrice = 0;
+    //  this.totalPrice = 0;
   },
 
   print() {
@@ -51,14 +51,3 @@ cart.add('Notebook', 50000, 3);
 cart.add('Macbook', 150000, 2);
 cart.add('Keyboard', 15000, 2);
 cart.print();
-
-// Продолжим работу с cart.js
-// Обнаружена уязвимость  нашем функционале
-// Если после добавления последнего товара присвоить к totalPrice любое значение,
-// например
-// cart.totalPrice = 10;
-// то при выводе print() общая стоимость корзины будет равна 10
-// Чтобы это предотвратить, необходимо свойство totalPrice сделать геттером который будет возвращать результат вызова метода calculateItemPrice
-// метод getTotalPrice больше не нужен
-// calculateItemPrice переделать таким образом, чтобы сумму он возвращал, а не записывал в свойство totalPrice
-// Вызов метода calculateItemPrice оставить только у геттера totalPrice
